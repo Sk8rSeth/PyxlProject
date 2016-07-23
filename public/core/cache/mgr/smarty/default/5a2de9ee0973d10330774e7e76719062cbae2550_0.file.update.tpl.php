40935cfd43e597cfe446aac07b045a1dc20e649b
Sk8rSeth <sethhowell2197@gmail.com>
2016-07-23 13:11:36 -0700

    modx setup

---
+++ public/core/cache/mgr/smarty/default/5a2de9ee0973d10330774e7e76719062cbae2550_0.file.update.tpl.php
@@ -0,0 +1,36 @@
+<?php /* Smarty version 3.1.27, created on 2016-07-23 20:03:38
+         compiled from "/var/www/public/manager/templates/default/element/template/update.tpl" */ ?>
+<?php
+/*%%SmartyHeaderCode:14180333045793cd9a6005b2_23514984%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '5a2de9ee0973d10330774e7e76719062cbae2550' => 
+    array (
+      0 => '/var/www/public/manager/templates/default/element/template/update.tpl',
+      1 => 1469249205,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '14180333045793cd9a6005b2_23514984',
+  'variables' => 
+  array (
+    'onTempFormPrerender' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => '3.1.27',
+  'unifunc' => 'content_5793cd9a615922_33291709',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_5793cd9a615922_33291709')) {
+function content_5793cd9a615922_33291709 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '14180333045793cd9a6005b2_23514984';
+?>
+<div id="modx-panel-template-div"></div>
+<?php echo $_smarty_tpl->tpl_vars['onTempFormPrerender']->value;
+
+}
+}
+?>
---
+++ public/core/cache/mgr/smarty/default/6f8a13b0c6a2dcc8d72786b6357a7d36948ded3b_0.file.create.tpl.php
@@ -0,0 +1,36 @@
+<?php /* Smarty version 3.1.27, created on 2016-07-23 20:04:01
+         compiled from "/var/www/public/manager/templates/default/element/template/create.tpl" */ ?>
+<?php
+/*%%SmartyHeaderCode:6947806995793cdb17296e0_89121763%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '6f8a13b0c6a2dcc8d72786b6357a7d36948ded3b' => 
+    array (
+      0 => '/var/www/public/manager/templates/default/element/template/create.tpl',
+      1 => 1469249205,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '6947806995793cdb17296e0_89121763',
+  'variables' => 
+  array (
+    'onTempFormPrerender' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => '3.1.27',
+  'unifunc' => 'content_5793cdb173e329_54434610',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_5793cdb173e329_54434610')) {
+function content_5793cdb173e329_54434610 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '6947806995793cdb17296e0_89121763';
+?>
+<div id="modx-panel-template-div"></div>
+<?php echo $_smarty_tpl->tpl_vars['onTempFormPrerender']->value;
+
+}
+}
+?>
