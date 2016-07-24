33460ac13a5400e3e1aac68a68aee904a2cd82a6
Sk8rSeth <sethhowell2197@gmail.com>
2016-07-23 22:47:37 -0700

    init new chunks for top slider and imported blog feed

---
+++ _resources/chunks/slider.php
@@ -0,0 +1,9 @@
+<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+  <ul class="orbit-container">
+    <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+    <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+    <li class="is-active orbit-slide">
+      <img class="orbit-image" src="assets/img/sample-quote1.png">
+    </li>
+  </ul>
+</div>
