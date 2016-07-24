285a6ac2874d7f26617865e7bb80e518e2e65169
Sk8rSeth <sethhowell2197@gmail.com>
2016-07-23 22:48:26 -0700

    groundwork for event tickets box and birds

--- _resources/templates/BigEarsSplash.php
+++ _resources/templates/BigEarsSplash.php
@@ -2,15 +2,18 @@
 <body>
 	<div class="hero">
 		<div class="row header">
-			<div class="be_logo large-6 columns"><img src="/assets/img/big-ears-2016-logo.png" alt=""></div>
-			<div class="slider large-6 columns">slider</div>
-		</div>
-		<div class="row event_container">
-			<div class="large-2 columns bird_left show-for-large-only">q</div>
-			<div class="large-centered columns event_box"></div>
-			<div class="large-2 columns bird_right show-for-large-only">q</div>
+			<div class="be_logo large-8 columns"><img src="/assets/img/big-ears-2016-logo.png" alt=""></div>
+			<div class="large-4 columns">[[$slider]]</div>
 		</div>
 	</div>
+		<div class="row event_container">
+			<div class="large-2 columns bird_left"><img src="/assets/img/big-ears-bird-left.png" alt=""></div>
+			<div class="large-8 columns event_box float-center">
+				<div class="large-6 column"><img src="/assets/img/buy-2016-pre-sale-tickets.png" alt=""></div>
+				<div class="large-6 column"><img src="/assets/img/see-last-years-event.png" alt=""></div>
+			</div>
+			<div class="large-2 columns bird_right .clearfix"><img src="/assets/img/big-ears-bird-right.png" alt=""></div>
+		</div>
 
 	<div class="feed_container">
 		[[$feed]]
