8dd13447b2e58eabf4f347815300169143905935
Sk8rSeth <sethhowell2197@gmail.com>
2016-07-24 18:07:47 -0700

    foundational construction of footer

--- _resources/chunks/footer.php
+++ _resources/chunks/footer.php
@@ -2,17 +2,16 @@
 	<div class="row">
 		<div class="large-3 columns"><h1>Stay Connected</h1></div>
 		<div class="large-6 columns">
-			<input type="email">
+			<input type="email" value="Email Address">
 			<div class="button">Sign Up</div>
 		</div>
-		<div class="large-3 columns">
-			<span class="social">fb</span>
-			<span class="social">twtr</span>
-			<span class="social">ig</span>
-		</div>
+		<div class="columns large-1 social fb"><img src="/assets/img/big-ears-facebook@2x.png" alt=""></div>
+		<div class="columns large-1 social tw"><img src="/assets/img/big-ears-twitter@2x.png" alt=""></div>
+		<div class="columns large-1 social ig"><img src="/assets/img/big-ears-instagram@2x.png" alt=""></div>
 	</div>
 </footer>
 </body>
 <script src="https://cdn.jsdelivr.net/foundation/6.2.3/foundation.min.js"></script>
+<script src="/assets/js/app.js"></script>
 
 </html>
