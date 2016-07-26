00540a1f26c2e3b42405d4a2703ca73e94dc9de6
Sk8rSeth <sethhowell2197@gmail.com>
2016-07-25 19:17:18 -0700

    updated desktop view of social icons, and link hover states

--- _resources/chunks/footer.php
+++ _resources/chunks/footer.php
@@ -1,11 +1,12 @@
 <footer>
 	<div class="row">
-		<div class="small-4 small-centered large-3 columns"><h1>Stay Connected</h1></div>
-		<div class="small-10 small-centered large-6 columns">
+		<div class="small-6 small-centered large-uncentered large-3 columns"><h1>Stay Connected</h1></div>
+		<div class="small-10 small-centered large-uncentered large-6 columns">
 			<input type="email" value="Email Address" />
 			<a href="" class="button">Sign Up</a>
 		</div>
-		<div class="columns small-offset-4 small-3 large-1 social fb"><img src="/assets/img/big-ears-facebook@2x.png" alt=""></div>
+
+		<div class="columns small-offset-4 small-3 large-1 large-offset-0 social fb"><img src="/assets/img/big-ears-facebook@2x.png" alt=""></div>
 		<div class="columns small-3 large-1 social tw"><img src="/assets/img/big-ears-twitter@2x.png" alt=""></div>
 		<div class="columns small-3 large-1 end social ig"><img src="/assets/img/big-ears-instagram@2x.png" alt=""></div>
 	</div>
