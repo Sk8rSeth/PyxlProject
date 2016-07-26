fe7b272a00d0a28a4e68cfa26b6ceba5567e7e0b
Sk8rSeth <sethhowell2197@gmail.com>
2016-07-25 17:48:26 -0700

    changed to reflect content insertion JIC multiple pages

--- _resources/chunks/footer.php
+++ _resources/chunks/footer.php
@@ -2,12 +2,12 @@
 	<div class="row">
 		<div class="large-3 columns"><h1>Stay Connected</h1></div>
 		<div class="large-6 columns">
-			<input type="email" value="Email Address">
-			<div class="button">Sign Up</div>
+			<input type="email" value="Email Address" />
+			<a href="" class="button">Sign Up</a>
 		</div>
 		<div class="columns large-1 social fb"><img src="/assets/img/big-ears-facebook@2x.png" alt=""></div>
 		<div class="columns large-1 social tw"><img src="/assets/img/big-ears-twitter@2x.png" alt=""></div>
-		<div class="columns large-1 social ig"><img src="/assets/img/big-ears-instagram@2x.png" alt=""></div>
+		<div class="columns large-1 end social ig"><img src="/assets/img/big-ears-instagram@2x.png" alt=""></div>
 	</div>
 </footer>
 </body>
