a96e3ebb8407802b220c2a450ff36ccfc2f15e5d
Sk8rSeth <sethhowell2197@gmail.com>
2016-07-25 18:48:21 -0700

    updated mobileview of footer and event buttons

--- _resources/chunks/footer.php
+++ _resources/chunks/footer.php
@@ -1,13 +1,13 @@
 <footer>
 	<div class="row">
-		<div class="large-3 columns"><h1>Stay Connected</h1></div>
-		<div class="large-6 columns">
+		<div class="small-4 small-centered large-3 columns"><h1>Stay Connected</h1></div>
+		<div class="small-10 small-centered large-6 columns">
 			<input type="email" value="Email Address" />
 			<a href="" class="button">Sign Up</a>
 		</div>
-		<div class="columns large-1 social fb"><img src="/assets/img/big-ears-facebook@2x.png" alt=""></div>
-		<div class="columns large-1 social tw"><img src="/assets/img/big-ears-twitter@2x.png" alt=""></div>
-		<div class="columns large-1 end social ig"><img src="/assets/img/big-ears-instagram@2x.png" alt=""></div>
+		<div class="columns small-offset-4 small-3 large-1 social fb"><img src="/assets/img/big-ears-facebook@2x.png" alt=""></div>
+		<div class="columns small-3 large-1 social tw"><img src="/assets/img/big-ears-twitter@2x.png" alt=""></div>
+		<div class="columns small-3 large-1 end social ig"><img src="/assets/img/big-ears-instagram@2x.png" alt=""></div>
 	</div>
 </footer>
 </body>
