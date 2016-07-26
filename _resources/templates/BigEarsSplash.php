3fbf7cb6755156c04f87569670cbf1e40bde186a
Sk8rSeth <sethhowell2197@gmail.com>
2016-07-25 18:13:10 -0700

    updated event/ticket buttons for better mobile layout

--- _resources/templates/BigEarsSplash.php
+++ _resources/templates/BigEarsSplash.php
@@ -3,13 +3,13 @@
 	<div class="hero">
 		<div class="row header">
 			<div class="be_logo small-12 large-7 columns"></div>
-			<div class="small-10 large-4 columns be_slider">[[$slider]]</div>
+			<div class="small-9 large-4 columns be_slider">[[$slider]]</div>
 		</div>
 	</div>
 		<div class="row event_container">
 			<div class="small-12 large-9 columns event_box float-center large-centered">
-				<span class="small-12 large-7 "><img src="/assets/img/buy-2016-pre-sale-tickets.png" alt=""></span>
-				<span class="small-12 large-7 "><img src="/assets/img/see-last-years-event.png" alt=""></span>
+				<span class="small-12 large-7 "><a href="#"><img src="/assets/img/buy-2016-pre-sale-tickets.png" alt=""></a></span>
+				<span class="small-12 large-7 "><a href="#"><img src="/assets/img/see-last-years-event.png" alt=""></a></span>
 			</div>
 			<div class="bird_left show-for-large"><img class="show-for-large" src="/assets/img/big-ears-bird-left.png" alt=""></div>
 			<div class="bird_right show-for-large"><img class="show-for-large" src="/assets/img/big-ears-bird-right.png" alt=""></div>
