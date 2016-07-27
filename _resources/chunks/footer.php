783153ec53a88c7f91ee91cc542277f29b998d03
Sk8rSeth <sethhowell2197@gmail.com>
2016-07-26 21:23:09 -0700

    added success/fail flash to email and send off to mailer class

--- _resources/chunks/footer.php
+++ _resources/chunks/footer.php
@@ -2,9 +2,9 @@
 	<div class="row align-middle">
 		<div class="small-6 small-centered large-uncentered large-3 columns"><h1>Stay Connected</h1></div>
 		<div class="email small-8 large-5 columns align-middle">
-			<input type="email" value="Email Address" />
+			<input type="email" placeholder="Email Address" />
 		</div>
-		<a href="" class="button columns small-3 large-1">SIGN UP</a>
+		<a href="[[~2]]" class="button columns small-3 large-1">SIGN UP</a>
 
 		<div class="columns small-8 large-2 social end">
 			<a href="" class="fb"></a>
@@ -15,6 +15,7 @@
 </footer>
 </body>
 <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.12.0-rc.2/jquery-ui.min.js" integrity="sha256-55Jz3pBCF8z9jBO1qQ7cIf0L+neuPTD1u7Ytzrp2dqo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/foundation/6.2.3/foundation.min.js"></script>
 <script src="/assets/js/app.js"></script>
 
