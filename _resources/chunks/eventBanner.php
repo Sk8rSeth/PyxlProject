87371221dbe182eae1407a42f0f92a4f18f97a09
Sk8rSeth <sethhowell2197@gmail.com>
2016-07-26 23:47:26 -0700

    reordered and restructured the event banner and chunk

---
+++ _resources/chunks/eventBanner.php
@@ -0,0 +1,8 @@
+<div class="row event_container">
+	<div class="small-12 large-9 columns event_box float-center large-centered">
+		<span class="small-12 small-centered large-7 "><a href="#"><img src="/assets/img/buy-2016-pre-sale-tickets.png" alt=""></a></span>
+		<span class="small-12 small-centered large-7 "><a href="#"><img src="/assets/img/see-last-years-event.png" alt=""></a></span>
+	</div>
+	<div class="bird_left show-for-large"><img class="show-for-large" src="/assets/img/big-ears-bird-left.png" alt=""></div>
+	<div class="bird_right show-for-large"><img class="show-for-large" src="/assets/img/big-ears-bird-right.png" alt=""></div>
+</div>
